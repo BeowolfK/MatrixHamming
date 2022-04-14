9e849af263b6d594bd6da124121499abeda9320f
BeowolfK0 <kenan.meylan@gmail.com>
2022-04-14 18:02:29 +0200

    some change of presentation

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def text_2_hamming(text, parity):
     encoded_string = []
     bin = to_binary(text)
     for index, letter in enumerate(bin):
-        print(f"Lettre a encoder : {text[index]}")
-        print(f"Binaire à encoder : {parity}{letter}")
+        print(f"Lettre a encoder :\t{text[index]}")
+        print(f"Binaire à encoder :\t{parity}{letter}")
         text_encode = hamming_encode(letter, parity)
-        print(f"Bits encodé aves la methode Hamming : {text_encode}")
+        print(f"Bits encodé aves la methode Hamming :\t{text_encode}")
         encoded_string.append(text_encode)
     return encoded_string
 
@@ -18,22 +18,22 @@ def text_2_hamming(text, parity):
 def hamming_2_text(encoded_string):
     decoded_string = []
     for binary in encoded_string:
-        print(f"Binaire a decoder : {binary}")
+        print(f"Binaire a decoder :\t{binary}")
         decoded_bytes = hamming_decode(binary)
-        print(f"Bits decodé aves la methode Hamming : {decoded_bytes}")
+        print(f"Bits decodé aves la methode Hamming :\t{decoded_bytes}")
         text_decode = to_string(decoded_bytes).replace("\x00", "")
-        print(f"Binaire décodé : {repr(text_decode)}")
+        print(f"Binaire décodé :\t{repr(text_decode)}")
         decoded_string.append(text_decode)
     return decoded_string
 
 
 def main():
     TEXT = "abc"
-    print(f"{'-' * 50}\nTexte a encoder : {repr(TEXT)}\n{'-' * 50}")
+    print(f"{'-' * 50}\nTexte a encoder\t:\t{repr(TEXT)}\n{'-' * 50}")
     encoded_string = text_2_hamming(TEXT, 1)
-    print(f"{'-' * 50}\nTexte encodé : {encoded_string}\n{'-' * 50}")
+    print(f"{'-' * 50}\nTexte encodé\t:\t{encoded_string}\n{'-' * 50}")
     decoded_string = repr(''.join(hamming_2_text(encoded_string)))
-    print(f"{'-' * 50}\nTexte decodé : {decoded_string}\n{'-' * 50}")
+    print(f"{'-' * 50}\nTexte decodé\t:\t{decoded_string}\n{'-' * 50}")
     print(
         "Le texte décodé est il identique au texte initial ? {}".format(
                 'Oui' if repr(TEXT) == decoded_string else 'Non'
@@ -46,16 +46,16 @@ def main():
     print(f"{'-' * 50}\nTexte encodé :\t\t{encoded_string}")
     print(f"Texte encodé corrompu : {corrupted_data}\n{'-' * 50}")
     decoded_string2 = repr(''.join(hamming_2_text(corrupted_data)))
-    print(f"{'-' * 50}\nTexte decodé : {decoded_string2}\n{'-' * 50}")
+    print(f"{'-' * 50}\nTexte decodé\t:\t{decoded_string2}\n{'-' * 50}")
     print(
         "Bytes identiques ? {}".format(
             'Oui' if encoded_string == corrupted_data else 'Non'
         )
     )
     print(
-        "Résultats identiques ? {}".format(
-            'Oui' if decoded_string == decoded_string2 else 'Non'
-        )
+            'SupercalifragilisticExpialidocious, les résultats sont identiques'
+            if decoded_string == decoded_string2 else
+            'Les résultats sont différents'
     )
 
 
